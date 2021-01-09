94afe9b9f71b4973747dcf202fb01e7cb84aa0af
Yoshihiro Tanaka <contact@cordea.jp>
2021-01-09 17:58:23 +0900

    restore the api key

--- src/js/options.js
+++ src/js/options.js
@@ -5,6 +5,10 @@ const API_KEY = "apiKey";
 const textField = new MDCTextField(document.querySelector(".mdc-text-field"));
 const button = document.getElementById("button");
 
+chrome.storage.sync.get([API_KEY], function (items) {
+    textField.value = items[API_KEY];
+});
+
 button.addEventListener("click", function () {
     const key = textField.value;
     if (key.length <= 0) {
