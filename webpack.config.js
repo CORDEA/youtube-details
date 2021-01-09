db584390092bb0ffd275b4033e6eb44dbd1f8ccc
Yoshihiro Tanaka <contact@cordea.jp>
2021-01-09 17:25:56 +0900

    also bundle the options.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/js/background.js',
+    entry: {
+        background: './src/js/background.js',
+        options: './src/js/options.js',
+    },
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'src/dist'),
     },
 };
