fef2c458d1011efc4cf42d7d8652717914d710dd
Yoshihiro Tanaka <contact@cordea.jp>
2021-01-11 15:44:02 +0900

    display info

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,3 +1,61 @@
 window.render = function (data) {
-    // TODO
+    const d = [
+        {
+            title: 'Details',
+            contents: [
+                {
+                    label: 'TITLE',
+                    content: data.snippet.title
+                },
+                {
+                    label: 'DESCRIPTION',
+                    content: data.snippet.description
+                },
+                {
+                    label: 'TAGS',
+                    content: data.snippet.tags.join(', ')
+                },
+                {
+                    label: 'PUBLISHED AT',
+                    content: data.snippet.publishedAt
+                },
+                {
+                    label: 'DURATION',
+                    content: data.contentDetails.duration
+                }
+            ]
+        },
+        {
+            title: 'Live Streaming Details',
+            contents: [
+                {
+                    label: 'STARTED AT',
+                    content: data.liveStreamingDetails.actualStartTime
+                },
+                {
+                    label: 'ENDED AT',
+                    content: data.liveStreamingDetails.actualEndTime
+                },
+                {
+                    label: 'CONCURRENT VIEWERS',
+                    content: data.liveStreamingDetails.concurrentViewers
+                }
+            ]
+        }
+    ];
+
+    d.forEach(function (item, i) {
+        const container = document.getElementById("container");
+        const containerNode = container.content.cloneNode(true);
+        containerNode.querySelector('.title').textContent = item.title;
+        document.body.appendChild(containerNode);
+
+        item.contents.forEach(function (content) {
+            const listItem = document.getElementById("list-item");
+            const listItemNode = listItem.content.cloneNode(true);
+            listItemNode.querySelector('.label').textContent = content.label;
+            listItemNode.querySelector('.content').textContent = content.content;
+            document.getElementsByClassName('list')[i].appendChild(listItemNode);
+        });
+    });
 }
